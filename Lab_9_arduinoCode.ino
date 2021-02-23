26e83c827ea6895f69a589dc7bc7cdb354ea8ec9
ShaneRandell <77622184+ShaneRandell@users.noreply.github.com>
2021-02-23 18:10:56 -0400

    Update Lab_9_arduinoCode.ino

--- Lab_9_arduinoCode.ino
+++ Lab_9_arduinoCode.ino
@@ -1,32 +1,37 @@
+/*---------LAB09-------------------*/
+
 #include <Servo.h>
 
-Servo servo1,servo2;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo servo1,servo2;  // creating objects for both servo motors connected to the ardiuno
+
+// Declaring both intial positions for the motors to start at
 
 int pos1 = 0;
 int pos2 = 0;
-int pyint;
-String pypos;
+
 
 void setup() 
 {
   Serial.begin(9600);
-  servo1.attach(9);
-  servo2.attach(10);// attaches the servo on pin 9 to the servo object
+  servo1.attach(9);  // attaches the servo on pin 9 
+  servo2.attach(10);  // attaches the servo on pin 10
 }
+
+/*This is the main loop for the code, it will check the serial to see if anything has been entered
+then the program will execute one of the if statements. and contorl the correct motor */
 
 void loop()
 {
   if(Serial.available() >0)
   {
     String data = Serial.readStringUntil('\n');
-    if (data == "M1")
+    if (data == "M1") // Motor 1
     {
       String data = Serial.readStringUntil('\n');
-      pos1 = data.toInt();
-      servo1.write(pos1);
+      pos1 = data.toInt();   //The string information need to be converted to a int IOT for the servo to read it 
+      servo1.write(pos1);   //writing the int angle value to the servo motor
     }
-    if (data == "M2")
+    if (data == "M2") // motor2
     {
       String data = Serial.readStringUntil('\n');
       pos1 = data.toInt();
@@ -34,7 +39,7 @@ void loop()
     }
     
     
-    Serial.flush();
+    Serial.flush(); //serial flush will clear the serial 
     delay(500);
     
     
